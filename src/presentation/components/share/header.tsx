df5e530c086558a0879faf002914addafcf738a2
Alberto Ortiz <baortiz98@outlook.com>
2024-11-06 21:22:02 -0500

    all primary buttons in header and dark/ligth mode added

--- src/presentation/components/share/header.tsx
+++ src/presentation/components/share/header.tsx
@@ -1,17 +1,22 @@
+import { paths } from "@/config/paths";
 import Link from "next/link";
+import ThmeSwitcher from "./theme-switcher";
 
 export default function Header() {
     return (
         <header className="w-full">
             <nav className="container mx-auto my-5 flex items-center justify-between">
-                <h1 className="font-semibold text-4xl">
-                    <span className="text-yellow-700">{"</>"}</span>
-                    albertonet
-                </h1>
+                <Link href={paths.home()}>
+                    <h1 className="font-semibold text-3xl">
+                        <span className="text-yellow-700">{"</>"}</span>
+                        albertonet
+                    </h1>
+                </Link>
                 <div className="flex items-center justify-center gap-4">
                     <Link href={""}>Portfolio</Link>
                     <Link href={""}>Blog</Link>
                     <Link href={""}>Contact</Link>
+                    <ThmeSwitcher />
                 </div>
             </nav>
         </header>
