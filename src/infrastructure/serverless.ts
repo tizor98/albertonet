9939f494c97255faef15e3cd34504d975fb7d621
Alberto Ortiz <baortiz98@outlook.com>
2024-11-11 15:53:59 -0500

    fix

--- src/infrastructure/serverless.ts
+++ src/infrastructure/serverless.ts
@@ -1,5 +1,8 @@
 import { LambdaClient } from "@aws-sdk/client-lambda";
 import outputs from "../../amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(outputs);
 
 export const SERVERLESS_SEND_MESSAGE_NAME = outputs.custom.FUNCTION_NAME ?? "";
 
