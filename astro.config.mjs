49d9acb57cfa430b880e76b8f9f6daf84ec56e0c
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2025-10-24 14:38:56 -0500

    chore: change provider to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import awsAmplify from "astro-aws-amplify";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import expressiveCode from "astro-expressive-code";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.albertonet.com",
     output: "server",
-    adapter: awsAmplify(),
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
     integrations: [
         expressiveCode({
             themes: ["dracula"],
